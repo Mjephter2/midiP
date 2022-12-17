3839da4aad06f08672c93258f16b9ef8ef7cc049
Jephter Maurice <mjephter2@gmail.com>
2022-12-16 20:28:29 -0800

    add package-info files for Sun Checkstyle compliance

--- src/main/java/sample/models/chords/package-info.java
+++ src/main/java/sample/models/chords/package-info.java
@@ -1,1 +1,14 @@
+/**
+ * Provides the classes implementing the various chords
+ * used in music.
+ * Implemented chords are:
+ *      Major Triad
+ *      Minor Triad
+ *      Major 7th
+ *      Minor 7th
+ *      Major 9th
+ *      Minor 9th
+ *      Dominant 7th
+ *      Dominant 9th
+ */
 package sample.models.chords;
---
+++ src/main/java/sample/models/exceptions/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides exception classes for detecting
+ * malformed musical components.
+ */
+package sample.models.exceptions;
