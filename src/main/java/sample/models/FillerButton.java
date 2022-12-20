7a4666faadc8e6b08d6b0306acb751605471b170
Jephter Maurice <mjephter2@gmail.com>
2022-12-19 17:31:59 -0800

    refactor to comply with Sun Checkstyle

--- src/main/java/sample/models/FillerButton.java
+++ src/main/java/sample/models/FillerButton.java
@@ -2,11 +2,20 @@ package sample.models;
 
 import javafx.scene.control.Button;
 
+/**
+ * Class implementing a Button used to fill space
+ * when designing a keyboard array.
+ */
 public class FillerButton extends Button {
 
-    public FillerButton(int width, int height){
+    /**
+     * Constructs a Filler Button with the given
+     * width and height.
+     * @param width width of the Button
+     * @param height height of the Button
+     */
+    public FillerButton(final int width, final int height) {
         this.setVisible(false);
         this.setPrefSize(width, height);
     }
-
 }
