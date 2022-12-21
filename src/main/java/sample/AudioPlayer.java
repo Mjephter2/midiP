409f1e7c9b11a355b1ed75207ae98123e0d54af2
Jephter Maurice <mjephter2@gmail.com>
2022-12-20 18:17:27 -0800

    src/main/java/sample/models/Note.java

--- src/main/java/sample/AudioPlayer.java
+++ src/main/java/sample/AudioPlayer.java
@@ -1,31 +1,42 @@
 package sample;
 
-import javax.sound.sampled.*;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.LineUnavailableException;
 import java.io.File;
 import java.io.IOException;
 
 /**
- * This is an example program that demonstrates how to play back an audio file
+ * Class to play back an audio file
  * using the SourceDataLine in Java Sound API.
  * @author www.codejava.net
- *
  */
 public class AudioPlayer {
 
-    // size of the byte buffer used to read/write the audio stream
+    /**
+     * size of the byte buffer used to read/write the audio stream.
+     */
     private static final int BUFFER_SIZE = 4096;
 
     /**
      * Play a given audio file.
      * @param audioFilePath Path of the audio file.
      */
-    public void play(String audioFilePath) {
+    public void play(final String audioFilePath) {
         File audioFile = new File(audioFilePath);
+        AudioInputStream audioStream;
+        AudioFormat format;
+        DataLine.Info info;
+        SourceDataLine audioLine;
         try {
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
-            AudioFormat format = audioStream.getFormat();
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-            SourceDataLine audioLine = (SourceDataLine) AudioSystem.getLine(info);
+            audioStream = AudioSystem.getAudioInputStream(audioFile);
+            format = audioStream.getFormat();
+            info = new DataLine.Info(SourceDataLine.class, format);
+            audioLine = (SourceDataLine) AudioSystem.getLine(info);
             audioLine.open(format);
 
             audioLine.start();
