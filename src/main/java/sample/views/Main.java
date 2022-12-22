5b2fdd970f712f1d2ca6243f3c66a6e26ddccab1
Jephter Maurice <mjephter2@gmail.com>
2022-12-21 17:45:42 -0800

    refactor to comply with Sun Checkstyle
    except for [MagicNumber] errors

--- src/main/java/sample/views/Main.java
+++ src/main/java/sample/views/Main.java
@@ -14,20 +14,42 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-    private BorderPane root = new BorderPane();
-    private GridPane center = new GridPane();
-    private Button mainWindowLearnButton = new Button("LEARN");
-    private Button mainWindowFreePlayButton = new Button("FREE\nPLAY");
-    Font mainWindowFont = new Font("Times New Roman",35);
+/**
+ * GUI for application main window.
+ */
+public final class Main extends Application {
+    /**
+     * Top Level pane.
+     */
+    private final BorderPane root = new BorderPane();
+
+    /**
+     * Center Pane.
+     */
+    private final GridPane center = new GridPane();
+
+    /**
+     * Button for opening the Scales / Chords learning window.
+     */
+    private final Button mainWindowLearnButton = new Button("LEARN");
+
+    /**
+     * Button for opening the Free Play window.
+     */
+    private final Button mainWindowFreePlayButton = new Button("FREE\nPLAY");
+
+    /**
+     * main Font in the main window.
+     */
+    private final Font mainWindowFont = new Font("Times New Roman", 35);
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(final Stage primaryStage) throws Exception {
         root.setStyle("-fx-background-color: lightgray;");
-        root.setPadding(new Insets(10,10,10,10));
+        root.setPadding(new Insets(10, 10, 10, 10));
         center.setHgap(2);
         center.setVgap(2);
-        center.setPadding(new Insets(0,30,2,30));
+        center.setPadding(new Insets(0, 30, 2, 30));
 
         Label info = new Label("");
         Label info2 = new Label("");
@@ -35,29 +57,38 @@ public class Main extends Application {
 
         Label mainWelcomeLabel = new Label("Welcome to Piano Display");
         root.setTop(mainWelcomeLabel);
-        mainWelcomeLabel.setFont(new Font("aerial",25));
-        BorderPane.setAlignment(mainWelcomeLabel,Pos.CENTER);
+        mainWelcomeLabel.setFont(new Font("aerial", 25));
+        BorderPane.setAlignment(mainWelcomeLabel, Pos.CENTER);
 
         mainWindowLearnButton.setFont(mainWindowFont);
-        mainWindowLearnButton.setStyle("-fx-text-fill: blue; -fx-background-radius: 30px; -fx-background-color: darkgray;");
-        mainWindowLearnButton.setPrefSize(200,200);
+        mainWindowLearnButton.setStyle(
+                "-fx-text-fill: blue;"
+                            + "-fx-background-radius: 30px;"
+                            + "-fx-background-color: darkgray;");
+        mainWindowLearnButton.setPrefSize(200, 200);
         mainWindowLearnButton.setOnMouseEntered(e -> {
-            mainWindowLearnButton.setPrefSize(210,210);
-            mainWindowFreePlayButton.setPrefSize(190,190);
-            mainWindowLearnButton.setStyle("-fx-text-fill: white; -fx-background-radius: 30px; -fx-background-color: green;");
+            mainWindowLearnButton.setPrefSize(210, 210);
+            mainWindowFreePlayButton.setPrefSize(190, 190);
+            mainWindowLearnButton.setStyle(
+                    "-fx-text-fill: white;"
+                            + "-fx-background-radius: 30px;"
+                            + "-fx-background-color: green;");
             info.setText("Learn");
             info2.setText("    ");
         });
         mainWindowLearnButton.setOnMouseExited(e -> {
-            mainWindowLearnButton.setPrefSize(200,200);
-            mainWindowFreePlayButton.setPrefSize(200,200);
-            mainWindowLearnButton.setStyle("-fx-text-fill: blue; -fx-background-radius: 30px;-fx-background-color: darkgray;");
+            mainWindowLearnButton.setPrefSize(200, 200);
+            mainWindowFreePlayButton.setPrefSize(200, 200);
+            mainWindowLearnButton.setStyle(
+                    "-fx-text-fill: blue;"
+                            + "-fx-background-radius: 30px;"
+                            + "-fx-background-color: darkgray;");
             info.setText("");
             info2.setText("");
         });
         mainWindowLearnButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
-            public void handle(MouseEvent e) {
+            public void handle(final MouseEvent e) {
                 LearnWindow learnWindow = new LearnWindow();
                 Stage chordStage = new Stage();
                 try {
@@ -71,26 +102,35 @@ public class Main extends Application {
             }
         });
 
-        mainWindowFreePlayButton.setStyle("-fx-text-fill: blue;-fx-background-radius: 30px; -fx-background-color: darkgray;");
-        mainWindowFreePlayButton.setPrefSize(200,200);
+        mainWindowFreePlayButton.setStyle(
+                "-fx-text-fill: blue;"
+                        + "-fx-background-radius: 30px;"
+                        + "-fx-background-color: darkgray;");
+        mainWindowFreePlayButton.setPrefSize(200, 200);
         mainWindowFreePlayButton.setFont(mainWindowFont);
         mainWindowFreePlayButton.setOnMouseEntered(e -> {
-            mainWindowFreePlayButton.setPrefSize(210,210);
-            mainWindowLearnButton.setPrefSize(190,190);
-            mainWindowFreePlayButton.setStyle("-fx-text-fill: white; -fx-background-radius: 30px;-fx-background-color: green;");
+            mainWindowFreePlayButton.setPrefSize(210, 210);
+            mainWindowLearnButton.setPrefSize(190, 190);
+            mainWindowFreePlayButton.setStyle(
+                    "-fx-text-fill: white;"
+                            + "-fx-background-radius: 30px;"
+                            + "-fx-background-color: green;");
             info.setText("Play freely");
             info2.setText("               ");
         });
         mainWindowFreePlayButton.setOnMouseExited(e -> {
-            mainWindowFreePlayButton.setPrefSize(200,200);
-            mainWindowLearnButton.setPrefSize(200,200);
-            mainWindowFreePlayButton.setStyle("-fx-text-fill: blue; -fx-background-radius: 30px;-fx-background-color: darkgray;");
+            mainWindowFreePlayButton.setPrefSize(200, 200);
+            mainWindowLearnButton.setPrefSize(200, 200);
+            mainWindowFreePlayButton.setStyle(
+                    "-fx-text-fill: blue;"
+                            + "-fx-background-radius: 30px;"
+                            + "-fx-background-color: darkgray;");
             info.setText("");
             info2.setText(info.getText());
         });
-        mainWindowFreePlayButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        mainWindowFreePlayButton.setOnMouseClicked(new EventHandler<>() {
             @Override
-            public void handle(MouseEvent e) {
+            public void handle(final MouseEvent e) {
                 FreePlayWindow chordWindowRoot = new FreePlayWindow();
                 Stage chordStage = new Stage();
                 try {
@@ -106,32 +146,32 @@ public class Main extends Application {
 
         root.setCenter(center);
         GridPane.setHalignment(center, HPos.CENTER);
-        center.add(mainWindowFreePlayButton,0,0);
-        center.add(mainWindowLearnButton,1,0);
+        center.add(mainWindowFreePlayButton, 0, 0);
+        center.add(mainWindowLearnButton, 1, 0);
 
         BorderPane bottom = new BorderPane();
         root.setBottom(bottom);
         Button exit = new Button("Exit");
         exit.setStyle("-fx-background-color: red; -fx-text-fill: white;");
-        exit.setPrefSize(35,35);
+        exit.setPrefSize(35, 35);
         exit.isScaleShape();
         exit.setOnMouseClicked(e -> System.exit(0));
         exit.setOnMouseEntered(e -> {
-            exit.setPrefSize(40,40);
+            exit.setPrefSize(40, 40);
             info.setText("Exit Application");
             info2.setText(info.getText());
         });
         exit.setOnMouseExited(e -> {
-            exit.setPrefSize(35,35);
+            exit.setPrefSize(35, 35);
             info.setText("");
             info2.setText(info.getText());
         });
         bottom.setCenter(exit);
-        BorderPane.setAlignment(exit,Pos.BOTTOM_CENTER);
+        BorderPane.setAlignment(exit, Pos.BOTTOM_CENTER);
         bottom.setRight(info);
-        BorderPane.setAlignment(info,Pos.BOTTOM_RIGHT);
+        BorderPane.setAlignment(info, Pos.BOTTOM_RIGHT);
         bottom.setLeft(info2);
-        BorderPane.setAlignment(info2,Pos.BOTTOM_LEFT);
+        BorderPane.setAlignment(info2, Pos.BOTTOM_LEFT);
 
         Scene scene = new Scene(root);
         primaryStage.setFullScreen(false);
@@ -141,8 +181,11 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
+    /**
+     * Application entry point.
+     * @param args ...
+     */
+    public static void main(final String[] args) {
         launch(args);
     }
 }
