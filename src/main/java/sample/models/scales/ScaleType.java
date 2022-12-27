53f88ad664de3035211a82eaa38f65a9c4c96cf6
Jephter Maurice <mjephter2@gmail.com>
2022-12-26 19:00:40 -0800

    add ScaleType to differentialte Scales

---
+++ src/main/java/sample/models/scales/ScaleType.java
@@ -0,0 +1,19 @@
+package sample.models.scales;
+
+/**
+ * enum for Scale types.
+ */
+public enum ScaleType {
+    /**
+     * Scale type for Major Scales.
+     */
+    MAJOR_SCALE,
+    /**
+     * Scale type for Minor Scales.
+     */
+    MINOR_SCALE,
+    /**
+     * Scale type for Major Pentatonic Scales.
+     */
+    MAJOR_PENTATONIC
+}
