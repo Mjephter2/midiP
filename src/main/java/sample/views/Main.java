12c6d79ccd4cc0bfcb22c5a6cc5e69791000fb64
Jephter Maurice <mjephter2@gmail.com>
2023-01-02 10:19:22 -0800

    add simple menu to Main GUI and refactor code to be more readable #40

--- src/main/java/sample/views/Main.java
+++ src/main/java/sample/views/Main.java
@@ -8,11 +8,21 @@ import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 /**
  * GUI for application main window.
@@ -29,9 +39,18 @@ public final class Main extends Application {
     private final GridPane center = new GridPane();
 
     /**
+     * VBox for the top portion of window.
+     */
+    private final VBox top = new VBox();
+
+    /**
      * Button for opening the Scales / Chords learning window.
      */
-    private final Button mainWindowLearnButton = new Button("LEARN\nCHORDS\nand\nSCALES");
+    private final Button mainWindowLearnButton = new Button(
+            "LEARN\n"
+            + "CHORDS\n"
+            + "and\n"
+            + "SCALES");
 
     /**
      * Button for opening the Free Play window.
@@ -43,25 +62,73 @@ public final class Main extends Application {
      */
     private final Font mainWindowFont = new Font("Times New Roman", 35);
 
+    /**
+     * Label to placed at bottom of window.
+     */
+    private final Label info = new Label("");
+
+    /**
+     * Welcome Label.
+     */
+    private final Label mainWelcomeLabel = new Label(
+            "Welcome to midiP!\nSelect one option below!");
+
     @Override
     public void start(final Stage primaryStage) throws Exception {
+        root.setStyle("-fx-background-color: lightgray;");
+        root.setPadding(new Insets(0, 10, 10, 10));
+        Scene scene = new Scene(root, 280, 700, Color.AQUA);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("midiP");
+        primaryStage.setScene(scene);
+
+        // create a menubar and add it to the top of root
+        MenuBar mb = new MenuBar();
+        mb.setUseSystemMenuBar(true);
+        root.setTop(top);
+        // create a menu
+        Menu m = new Menu("midiP");
+        // create menuitems and add actions
+        MenuItem aboutMidiP = new MenuItem("About midiP");
+        aboutMidiP.setOnAction(action -> {
+            try {
+                Desktop.getDesktop().browse(
+                        new URL("https://github.com/Mjephter2/midiP").toURI());
+            } catch (IOException | URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        MenuItem quitMidiP = new MenuItem("Quit midiP");
+        quitMidiP.setOnAction(action -> {
+            System.exit(0);
+        });
+        // add menu items to menu
+        m.getItems().add(aboutMidiP);
+        m.getItems().add(quitMidiP);
+        // add menu to menubar
+        mb.getMenus().add(m);
+        top.getChildren().add(mb);
+
+        // style center grid and add components
+        center.setHgap(5);
+        center.setVgap(5);
+        center.setPadding(new Insets(0, 30, 2, 30));
+        center.setAlignment(Pos.CENTER);
+        center.add(mainWelcomeLabel, 0, 0);
+        center.add(mainWindowFreePlayButton, 0, 1);
+        center.add(mainWindowLearnButton, 0, 2);
+        GridPane.setHalignment(center, HPos.CENTER);
+        GridPane.setValignment(center, VPos.CENTER);
+
+        // style welcome label
+        mainWelcomeLabel.setTextAlignment(TextAlignment.CENTER);
+        mainWelcomeLabel.setWrapText(true);
+        mainWelcomeLabel.setFont(new Font("aerial", 25));
+        GridPane.setHalignment(mainWelcomeLabel, HPos.CENTER);
+
+        // style learn button
         mainWindowLearnButton.setTextAlignment(TextAlignment.CENTER);
         mainWindowLearnButton.setWrapText(true);
-        root.setStyle("-fx-background-color: lightgray;");
-        root.setPadding(new Insets(10, 10, 10, 10));
-        center.setHgap(2);
-        center.setVgap(2);
-        center.setPadding(new Insets(0, 30, 2, 30));
-
-        Label info = new Label("");
-
-        Label mainWelcomeLabel = new Label("Welcome to midiP\n"
-                + "Select one option below!");
-        mainWelcomeLabel.setTextAlignment(TextAlignment.CENTER);
-        root.setTop(mainWelcomeLabel);
-        mainWelcomeLabel.setFont(new Font("aerial", 25));
-        BorderPane.setAlignment(mainWelcomeLabel, Pos.TOP_CENTER);
-
         mainWindowLearnButton.setFont(mainWindowFont);
         mainWindowLearnButton.setStyle("-fx-background-radius: 30px;"
                             + "-fx-background-color: darkgray;");
@@ -71,7 +138,6 @@ public final class Main extends Application {
                     "-fx-text-fill: white;"
                             + "-fx-background-radius: 30px;"
                             + "-fx-background-color: #010B02;");
-//            mainWindowLearnButton.setBackground(new Background( new BackgroundImage(new Image("file:/test.png"), null, null, null, new BackgroundSize(100, 100, true, true, true, false))));
             info.setText("Learn");
         });
         mainWindowLearnButton.setOnMouseExited(e -> {
@@ -92,6 +158,7 @@ public final class Main extends Application {
             }
         });
 
+        // style free play button
         mainWindowFreePlayButton.setStyle(
                 "-fx-background-radius: 30px;"
                         + "-fx-background-color: darkgray;");
@@ -123,24 +190,13 @@ public final class Main extends Application {
             }
         });
 
-        root.setCenter(center);
-        GridPane.setHalignment(center, HPos.CENTER);
-        GridPane.setValignment(center, VPos.CENTER);
-        center.setAlignment(Pos.CENTER);
-        center.add(mainWindowFreePlayButton, 0, 0);
-        center.add(mainWindowLearnButton, 0, 1);
-
+        // add label at bottom of window
         BorderPane bottom = new BorderPane();
-        root.setBottom(bottom);
         bottom.setRight(info);
+        root.setCenter(center);
+        root.setBottom(bottom);
         BorderPane.setAlignment(info, Pos.BOTTOM_RIGHT);
 
-        root.setPrefSize(280, 500);
-        Scene scene = new Scene(root);
-        primaryStage.setFullScreen(false);
-        primaryStage.setResizable(false);
-        primaryStage.setTitle("midiP");
-        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
