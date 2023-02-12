725570d9047695a64ce82984469da2318b49b01d
Jephter Maurice <mjephter2@gmail.com>
2023-02-12 12:16:17 -0800

    implement Scale modes

--- src/main/java/sample/models/scales/Scale.java
+++ src/main/java/sample/models/scales/Scale.java
@@ -1,12 +1,17 @@
 package sample.models.scales;
 
+import sample.models.Invertable;
 import sample.models.Note;
 import sample.models.Transposable;
+import sample.models.exceptions.InvalidNoteException;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Class representation of a Scale.
  */
-public class Scale extends Transposable {
+public class Scale extends Transposable implements Invertable {
     /**
      * Type of the Scale.
      */
@@ -64,5 +69,89 @@ public class Scale extends Transposable {
             }
             default -> throw new Exception("Chord type is unrecognized or not yet implemented!");
         }
+    }
+
+    /**
+     * Ionian Mode is equivalent to the root Major Scale.
+     * @return the original Scale
+     */
+    public Note[] generateIonianMode() {
+        return scaleNotes;
+    }
+
+    /**
+     * @return the Notes of the Scale Dorian Mode
+     */
+    public Note[] generateDorianMode() {
+        return generateMode(ScaleMode.DORIAN);
+    }
+
+    /**
+     * @return the Notes of the Scale Phrygian Mode
+     */
+    public Note[] generatePhrygianMode() {
+        return generateMode(ScaleMode.PHRYGIAN);
+    }
+
+    /**
+     * @return the Notes of the Scale Lydian Mode
+     */
+    public Note[] generateLydianMode() {
+        return generateMode(ScaleMode.LYDYAN);
+    }
+
+    /**
+     * @return the Notes of the Scale Mixolydian Mode
+     */
+    public Note[] generateMixolydianMode() {
+        return generateMode(ScaleMode.MIXOLYDIAN);
+    }
+
+    /**
+     * @return the Notes of the Scale Aeolian Mode
+     */
+    public Note[] generateAeolianMode() {
+        return generateMode(ScaleMode.AEOLIAN);
+    }
+
+    /**
+     * @return the Notes of the Scale Locrian Mode
+     */
+    public Note[] generateLocrianMode() {
+        return generateMode(ScaleMode.LOCRIAN);
+    }
+
+    /**
+     * @param mode mode of the scale
+     * @return the Scale Notes with the given Mode
+     */
+    private Note[] generateMode(final ScaleMode mode) {
+        return invert(mode.getValue());
+    }
+
+    /**
+     *
+     * @param index the index of the scale note to start mode from.
+     * @return the Scale Notes starting from the index specified
+     */
+    @Override
+    public Note[] invert(int index) {
+        if (index >= scaleNotes.length) {
+            return scaleNotes;
+        }
+        Note[] uniqueNotes = Arrays.copyOfRange(scaleNotes, 1, scaleNotes.length);
+        Note[] firstNotes = Arrays.copyOfRange(uniqueNotes, index - 1, uniqueNotes.length);
+        List<Note> lastNotes = Arrays.stream(Arrays.copyOfRange(uniqueNotes, 0, index - 1)).toList();
+        List<Note> sharpLastNotes = lastNotes.stream().map(note -> {
+            try {
+                return note.sharp(12);
+            } catch (InvalidNoteException e) {
+                throw new RuntimeException(e);
+            }
+        }).toList();
+        Note[] invertedNotes = new Note[uniqueNotes.length];
+        System.arraycopy(firstNotes, 0, invertedNotes, 0, firstNotes.length);
+        System.arraycopy(sharpLastNotes.toArray(), 0, invertedNotes, firstNotes.length, lastNotes.size());
+        return invertedNotes;
     }
 }
--- src/test/java/tst/models/ScaleTest.java
+++ src/test/java/tst/models/ScaleTest.java
@@ -83,4 +83,73 @@ class ScaleTest {
     public void minorPentatonicScaleOutbound() {
         assertThrows(InvalidNoteException.class, () -> new Scale(ScaleType.MINOR_PENTATONIC, new Note("F7")));
     }
+
+    @Test
+    public void scaleModesTest() throws Exception {
+        Scale cMajorScale = new Scale(ScaleType.MAJOR_SCALE, new Note("C3"));
+
+        Note[] ionianMode = cMajorScale.generateIonianMode();
+        assertEquals("C3", ionianMode[0].toString());
+        assertEquals("D3", ionianMode[1].toString());
+        assertEquals("E3", ionianMode[2].toString());
+        assertEquals("F3", ionianMode[3].toString());
+        assertEquals("G3", ionianMode[4].toString());
+        assertEquals("A3", ionianMode[5].toString());
+        assertEquals("B3", ionianMode[6].toString());
+        assertEquals("C4", ionianMode[7].toString());
+
+        Note[] dorianMode = cMajorScale.generateDorianMode();
+        assertEquals("D3", dorianMode[0].toString());
+        assertEquals("E3", dorianMode[1].toString());
+        assertEquals("F3", dorianMode[2].toString());
+        assertEquals("G3", dorianMode[3].toString());
+        assertEquals("A3", dorianMode[4].toString());
+        assertEquals("B3", dorianMode[5].toString());
+        assertEquals("C4", dorianMode[6].toString());
+
+        Note[] phrygianMode = cMajorScale.generatePhrygianMode();
+        assertEquals("E3", phrygianMode[0].toString());
+        assertEquals("F3", phrygianMode[1].toString());
+        assertEquals("G3", phrygianMode[2].toString());
+        assertEquals("A3", phrygianMode[3].toString());
+        assertEquals("B3", phrygianMode[4].toString());
+        assertEquals("C4", phrygianMode[5].toString());
+        assertEquals("D4", phrygianMode[6].toString());
+
+        Note[] lydianMode = cMajorScale.generateLydianMode();
+        assertEquals("F3", lydianMode[0].toString());
+        assertEquals("G3", lydianMode[1].toString());
+        assertEquals("A3", lydianMode[2].toString());
+        assertEquals("B3", lydianMode[3].toString());
+        assertEquals("C4", lydianMode[4].toString());
+        assertEquals("D4", lydianMode[5].toString());
+        assertEquals("E4", lydianMode[6].toString());
+
+        Note[] mixolydianMode = cMajorScale.generateMixolydianMode();
+        assertEquals("G3", mixolydianMode[0].toString());
+        assertEquals("A3", mixolydianMode[1].toString());
+        assertEquals("B3", mixolydianMode[2].toString());
+        assertEquals("C4", mixolydianMode[3].toString());
+        assertEquals("D4", mixolydianMode[4].toString());
+        assertEquals("E4", mixolydianMode[5].toString());
+        assertEquals("F4", mixolydianMode[6].toString());
+
+        Note[] aeolianMode = cMajorScale.generateAeolianMode();
+        assertEquals("A3", aeolianMode[0].toString());
+        assertEquals("B3", aeolianMode[1].toString());
+        assertEquals("C4", aeolianMode[2].toString());
+        assertEquals("D4", aeolianMode[3].toString());
+        assertEquals("E4", aeolianMode[4].toString());
+        assertEquals("F4", aeolianMode[5].toString());
+        assertEquals("G4", aeolianMode[6].toString());
+
+        Note[] locrianMode = cMajorScale.generateLocrianMode();
+        assertEquals("B3", locrianMode[0].toString());
+        assertEquals("C4", locrianMode[1].toString());
+        assertEquals("D4", locrianMode[2].toString());
+        assertEquals("E4", locrianMode[3].toString());
+        assertEquals("F4", locrianMode[4].toString());
+        assertEquals("G4", locrianMode[5].toString());
+        assertEquals("A4", locrianMode[6].toString());
+    }
 }
